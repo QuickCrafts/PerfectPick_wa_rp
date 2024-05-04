dd99a9b9fdb55fa1c2059b92820f833bed2be456
Paula Guzmán <pauladaguz@gmail.com>
2024-05-04 12:51:09 -0500

    Advance Automatic

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker build -t PerfectPick_int_rp .
+docker build -t perfectpick_int_rp .
 
-docker run --network perfectpicknetwork -p 77:77 PerfectPick_int_rp
+docker run --network perfectpicknetwork -p 77:77 perfectpick_int_rp
